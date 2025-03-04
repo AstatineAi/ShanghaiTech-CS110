f25da423c8c1a15df5f23b7d4e2d1b2f026b01b1
Hengyu Ai <aihy2023@shanghaitech.edu.cn>
2025-03-04 20:28:27 +0800

    docs(Lab): comment for lab3_ex2

--- Lab/Lab03/lab3_ex2.s
+++ Lab/Lab03/lab3_ex2.s
@@ -21,18 +21,18 @@ dest:
 
 .text
 main:
-	addi t0, x0, 0
-	la t1, source
-	la t2, dest
+	addi t0, x0, 0   # add immediate        t0 = x0 + 0, that is, set t0 to 0
+	la t1, source    # load address         t1 = &source
+	la t2, dest	     # load address         t2 = &dest
 loop:
-	slli t3, t0, 2
-	add t4, t1, t3
-	lw t5, 0(t4)
-	beq t5, x0, exit
-	add t6, t2, t3
-	sw t5, 0(t6)
-	addi t0, t0, 1
-	jal x0, loop
+	slli t3, t0, 2   # shift left immediate t3 = t0 << 2, that is, t3 = t0 * 4
+	add t4, t1, t3   # add                  t4 = t1 + t3
+	lw t5, 0(t4)     # load word            t5 = *t4
+	beq t5, x0, exit # branch equal         t5 == x0, if *t4 == 0, jump to label exit
+	add t6, t2, t3   # add                  t6 = t2 + t3
+	sw t5, 0(t6)     # store word           *t6 = t5
+	addi t0, t0, 1   # add immediate        t0 = t0 + 1
+	jal x0, loop     # jump and link        goto label loop
 exit:
 	jal ra, print_lists
 	addi a0, x0, 10
