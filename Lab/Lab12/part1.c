1fa7d557ac380e986236ce96abf147b8f1c548e4
Hengyu Ai <aihy2023@shanghaitech.edu.cn>
2025-05-12 13:53:41 +0800

    init(Lab): Lab12 initial commit

---
+++ Lab/Lab12/part1.c
@@ -0,0 +1,49 @@
+#include <immintrin.h> /* header file for the SSE intrinsics */
+#include <assert.h>
+#include <stdio.h>
+
+int **a;
+int **b;
+int **c;
+
+
+void naive_shift_right(void){
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            c[i][j] = a[i][j] >> b[i][j];
+        }
+    }
+}
+
+void simd_shift_right(void) {
+    // TODO: implement me!
+}
+
+
+int main(void) {
+    a = malloc(1000 * sizeof(int *));
+    b = malloc(1000 * sizeof(int *));
+    c = malloc(1000 * sizeof(int *));
+    for (int i = 0; i < 1000; ++i) {
+        a[i] = malloc(40 * sizeof(int));
+        b[i] = malloc(40 * sizeof(int));
+        c[i] = malloc(40 * sizeof(int));
+    }
+
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            a[i][j] = i * 40 + j;
+            b[i][j] = i * 11 % 8;
+        }
+    }
+
+    simd_shift_right();
+
+    for (int i = 0; i < 1000; ++i) {
+        for (int j = 0; j < 40; ++j) {
+            assert(c[i][j] == a[i][j] >> b[i][j]);
+        }
+    }
+    printf("PASS\n");
+    return 0;
+}
